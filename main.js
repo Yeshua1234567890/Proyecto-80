a14b251a8827caa6b2d114622230219576d8cd9c
Yeshua1234567890 <96255363+Yeshua1234567890@users.noreply.github.com>
2022-04-21 12:21:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+nombre_alumno_array=[];
+
+
+function submit()
+{
+    var ver_alumno_array=[];
+
+    for (var j=1; j j<=4; j++)
+    {}
+    var nombre_alumno=document.getElementById("nombre_alumno_"+j).ariaValueMax;
+    console.log(nombre_alumno);
+    nombre_alumno_array.push(nombre_alumno);
+}
